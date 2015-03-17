179f7285ce057a7a1e0768bd307077e29b8554bd
eagraf <graf.ethan@gmail.com>
2015-03-17 19:44:51 -0400

    Added Google Maps support

--- app/src/main/java/graf/ethan/alzheimers_project/MainActivity.java
+++ app/src/main/java/graf/ethan/alzheimers_project/MainActivity.java
@@ -1,20 +1,34 @@
 package graf.ethan.alzheimers_project;
 
 import android.content.Context;
+import android.content.Intent;
+import android.location.Address;
 import android.location.Criteria;
+import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.common.api.Api;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.maps.GoogleMap;
+
+import java.io.IOException;
+import java.util.ArrayList;
 
 
 public class MainActivity extends ActionBarActivity implements LocationListener {
     private TextView latituteField;
     private TextView longitudeField;
+    private TextView addressField;
     private LocationManager locationManager;
+    private Geocoder geoCoder;
     private String provider;
 
     /** Called when the activity is first created. */
@@ -24,6 +38,7 @@ public class MainActivity extends ActionBarActivity implements LocationListener 
         setContentView(R.layout.activity_main);
         latituteField = (TextView) findViewById(R.id.TextViewLatitudeCoordinate);
         longitudeField = (TextView) findViewById(R.id.TextViewLongitudeCoordinate);
+        addressField = (TextView) findViewById(R.id.TextViewAddressString);
 
         // Get the location manager
         locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
@@ -33,6 +48,8 @@ public class MainActivity extends ActionBarActivity implements LocationListener 
         provider = locationManager.getBestProvider(criteria, false);
         Location location = locationManager.getLastKnownLocation(provider);
 
+        geoCoder = new Geocoder(this);
+
         // Initialize the location fields
         if (location != null) {
             System.out.println("Provider " + provider + " has been selected.");
@@ -40,6 +57,7 @@ public class MainActivity extends ActionBarActivity implements LocationListener 
         } else {
             latituteField.setText("Location not available");
             longitudeField.setText("Location not available");
+            addressField.setText("Address not available");
         }
     }
 
@@ -59,10 +77,23 @@ public class MainActivity extends ActionBarActivity implements LocationListener 
 
     @Override
     public void onLocationChanged(Location location) {
-        int lat = (int) (location.getLatitude());
-        int lng = (int) (location.getLongitude());
+        double lat = location.getLatitude();
+        double lng = location.getLongitude();
+        Address address = null;
+        try {
+            address = ((ArrayList<Address>) geoCoder.getFromLocation(location.getLatitude(), location.getLongitude(), 16)).get(0);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
         latituteField.setText(String.valueOf(lat));
         longitudeField.setText(String.valueOf(lng));
+        if(address != null) {
+            addressField.setText(address.getAddressLine(0));
+        }
+        else {
+            addressField.setText("Address not available");
+        }
     }
 
     @Override
@@ -80,5 +111,10 @@ public class MainActivity extends ActionBarActivity implements LocationListener 
     public void onProviderDisabled(String provider) {
         Toast.makeText(this, "Disabled provider " + provider,
                 Toast.LENGTH_SHORT).show();
+    }
+
+    public void openMap(View view) {
+        Intent intent = new Intent(this, MapActivity.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/graf/ethan/alzheimers_project/MapActivity.java
@@ -0,0 +1,14 @@
+package graf.ethan.alzheimers_project;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.google.android.gms.maps.SupportMapFragment;
+
+public class MapActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map);
+    }
+}
